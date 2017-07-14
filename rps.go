b99683f283c81a5d0dabb930767c4948194f342c
M <mtso@tuta.io>
2017-07-14 15:15:31 -0700

    Rename `inputchoices` -> `newChoices`
    Rename input variable for better documentation from function signature.
    
    Also add comments for better documentation.

--- rps.go
+++ rps.go
@@ -1,3 +1,9 @@
+// Package rps is a random choice picker.
+//
+// Defaults to {"rock", "paper", "scissors"},
+// but can supply a slice of strings to choose
+// from in Roll() or use Set() to change the
+// choices for all subsequent calls to Roll().
 package rps
 
 import (
@@ -13,12 +19,10 @@ var defaultChoices = []string{
 
 // Default choices {"rock", "paper", "scissors"}.
 var choices = defaultChoices
-
 var choicelen = len(choices)
 
-var source = rand.NewSource(time.Now().Unix())
-
 // Random number generator.
+var source = rand.NewSource(time.Now().Unix())
 var rng = rand.New(source)
 
 func randomInt(end int) int {
@@ -26,6 +30,9 @@ func randomInt(end int) int {
 	return num % end
 }
 
+// Roll returns a random choice from an array.
+// (Defaults to {"rock", "paper", "scissors"}.
+// Optionally pass a string slice to pick from.
 func Roll(custom ...[]string) string {
 	if len(custom) > 0 {
 		choices := custom[0]
@@ -37,7 +44,8 @@ func Roll(custom ...[]string) string {
 	return choices[choice]
 }
 
-func Set(inputchoices []string) {
-	choices = inputchoices
-	choicelen = len(inputchoices)
+// Set changes the choices for subsequent Roll() calls.
+func Set(newChoices []string) {
+	choices = newChoices
+	choicelen = len(newChoices)
 }
